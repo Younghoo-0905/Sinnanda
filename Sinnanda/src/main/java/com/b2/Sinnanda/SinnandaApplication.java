3e3efe0b0dbfbcc2e052b0ef3348fa8ce7cb9f98
dnjsgml1199@gmail.com <zhffkEnql12>
2021-12-08 11:25:54 +0900

    [이원희]깃 연동

---
+++ Sinnanda/src/main/java/com/b2/Sinnanda/SinnandaApplication.java
@@ -0,0 +1,13 @@
+package com.b2.Sinnanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinnandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinnandaApplication.class, args);
+	}
+
+}
