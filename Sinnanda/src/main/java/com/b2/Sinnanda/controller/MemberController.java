cc0c5b6bf0f8da012ef782b742690a9a38e3deb8
Yoo Dong Jin <lucky7yoo@naver.com>
2021-12-09 11:55:45 +0900

    [유동진] memberController 회원정보수정, myPage.jsp생성

--- Sinnanda/src/main/java/com/b2/Sinnanda/controller/MemberController.java
+++ Sinnanda/src/main/java/com/b2/Sinnanda/controller/MemberController.java
@@ -1,8 +1,33 @@
 package com.b2.Sinnanda.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.b2.Sinnanda.service.MemberService;
+import com.b2.Sinnanda.vo.Member;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class MemberController {
-
+	@Autowired MemberService memberService;
+	
+	// 회원 정보 수정
+	@GetMapping("/modifyMember")
+	public String modifyMember(Model model, Member member) {
+		log.debug("MemberController : memberNo -> " + member.getMemberNo());
+		model.addAttribute("memberNo", member.getMemberNo());
+		return "modifyMember";
+	}
+	@PostMapping("/modifyMember")
+	public String modifyMember(Member member) {
+		log.debug("MemberController : modifyMember -> " + member.toString());
+		memberService.modifyMember(member);
+		log.debug("MemberController : 수정 성공!");
+		return "redirect:/memberList";
+	}
 }
---
+++ Sinnanda/src/main/webapp/WEB-INF/jsp/myPage.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>마이페이지(회원)</title>
+</head>
+<body>
+	<h1>마이 페이지</h1>
+	<div>
+		<a href = "/modifyMember">회원정보 수정</a>
+		<a href = "">예약내역 확인</a>
+		<a href = "">보유쿠폰 확인</a>
+		<a href = "">작성한 리뷰 보기</a>
+		<a href = "">작성한 QnA 보기</a>
+		<a href = "">탈퇴하기</a>
+	</div>
+</body>
+</html>
